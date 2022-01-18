ca244859e8cac9d0f5d4317c8f6903fddd882051
Gaurav Amarnani <java.gaurav12@gmail.com>
2022-01-18 08:15:55 +0530

    Time: 5 ms (36.35%), Space: 47 MB (11.23%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -4,16 +4,36 @@ class Solution {
         int n2 = nums2.length;
         int[] merged = new int[n1 + n2];
         int i = 0, j = 0, k = 0;
-        while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j])
-                merged[k++] = nums1[i++];
-            else
+        if(n1 == 0)
+            while (j < n2)
                 merged[k++] = nums2[j++];
+        else if(n2 == 0)
+            while (i < n1)
+                merged[k++] = nums1[i++];
+        else {
+            if (nums1[n1 - 1] < nums2[0]) {
+                while (i < n1)
+                    merged[k++] = nums1[i++];
+                while (j < n2)
+                    merged[k++] = nums2[j++];
+            } else if (nums2[n2 - 1] < nums1[0]) {
+                while (j < n2)
+                    merged[k++] = nums2[j++];
+                while (i < n1)
+                    merged[k++] = nums1[i++];
+            } else {
+                while (i < n1 && j < n2) {
+                    if (nums1[i] < nums2[j])
+                        merged[k++] = nums1[i++];
+                    else
+                        merged[k++] = nums2[j++];
+                }
+                while (i < n1)
+                    merged[k++] = nums1[i++];
+                while (j < n2)
+                    merged[k++] = nums2[j++];
+            }
         }
-        while (i < n1)
-            merged[k++] = nums1[i++];
-        while (j < n2)
-            merged[k++] = nums2[j++];
         if (merged.length % 2 != 0)
             return merged[merged.length / 2];
         else {

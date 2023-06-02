5eccd78ae7b0d14a49ef564e8977f4bea833a4fe
Gaurav Amarnani <java.gaurav12@gmail.com>
2023-06-02 10:25:33 +0530

    Time: 2 ms (89.76%), Space: 40.8 MB (44.43%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,32 +1,32 @@
 class Solution {
-    public boolean isValid(String str) {
-        ArrayList<String> parenthesis = new ArrayList<>();
-        parenthesis.add(0, "{");
-        parenthesis.add(1, "}");
-        parenthesis.add(2, "(");
-        parenthesis.add(3, ")");
-        parenthesis.add(4, "[");
-        parenthesis.add(5, "]");
-        ArrayList<Integer> stack = new ArrayList<>();
-        for(int i = 0; i < str.length(); i++) {
-            String temp = str.charAt(i) + "";
-            if(parenthesis.contains(temp)) {
-                if(parenthesis.indexOf(temp) % 2 != 0) {
-                    if(stack.size()==0)
-                        return false;
-                    if (stack.get(stack.size() - 1) == parenthesis.indexOf(temp) - 1) {
-                        String current = stack.get(stack.size() - 1) + "";
-                        int index = stack.lastIndexOf(Integer.parseInt(stack.get(stack.size() - 1) + ""));
-                        stack.remove(index);
-                    } else {
-                        return false;
-                    }
-                }
-                else {
-                    stack.add(parenthesis.indexOf(temp));
-                }
+    public boolean handleClosing(Stack<Character> s, char openBracket){
+        if(s.size() == 0){
+            return false;
+        }else if(s.peek() != openBracket){
+            return false;
+        }else {
+            s.pop();
+        }
+        return true;
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else if(ch == ')'){
+                boolean val = handleClosing(stack,'(');
+                if(val == false) return false;
+            }else if(ch == '}'){
+                boolean val = handleClosing(stack,'{');
+                if(val == false) return false;
+            }else if(ch == ']'){
+                boolean val = handleClosing(stack,'[');
+                if(val == false) return false;
             }
         }
-        return stack.size() == 0;
+        
+        if(stack.size() > 0)return false;
+        return true;
     }
 }

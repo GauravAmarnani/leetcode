755936905fa68c4da4e01ecced5348cdd8d3175a
Gaurav Amarnani <java.gaurav12@gmail.com>
2024-04-20 08:33:58 +0530

    Time: 1 ms (49.23%), Space: 57.2 MB (67.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,9 +1,32 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] nums2 = new int[nums.length];
-        for(int i = 0; i < nums.length; i++)
-            nums2[(i+k) % nums.length] = nums[i];
-        for(int i = 0; i < nums.length; i++)
-            nums[i] = nums2[i];
+        //rotateOnSameArray(nums, k);
+        rotateOnAnotherArray(nums, k);
+    }
+
+    public void rotateOnSameArray(int[] nums, int k) {
+        for(int i = 0; i < k; i++) {
+            int f = nums[nums.length-1];
+            for(int j = nums.length-1; j > 0; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = f;
+            System.out.println("I" + i + " = " + Arrays.toString(nums));
+        }
+    }
+
+    public void rotateOnAnotherArray(int[] nums, int k) {
+        if(nums.length <= 1)
+            return;
+        int[] another = new int[nums.length];
+        k = k % nums.length;
+        int index = nums.length - k;
+        int temp = 0;
+        for(int i = index; i < nums.length; i++)
+            another[temp++] = nums[i];
+        for(int i = 0; i < index; i++) 
+            another[temp++] = nums[i];
+        for(int i = 0; i < another.length; i++) 
+            nums[i] = another[i];
     }
 }

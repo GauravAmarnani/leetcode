db519a5caac50035ef5a2b9d7e4371ca05e6f638
Gaurav Amarnani <java.gaurav12@gmail.com>
2024-04-20 09:35:52 +0530

    Time: 21 ms (8.90%), Space: 48.5 MB (98.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,7 +2,15 @@ import java.util.Arrays;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+            if (map.containsKey(num) && map.get(num) > nums.length/2)
+                return num;
+        }
+        return -1;
     }
 }
